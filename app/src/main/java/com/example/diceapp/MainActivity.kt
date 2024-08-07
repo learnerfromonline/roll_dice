7d109be7ba7910b38976019386f1796e630d84ec
learnerfromonline <satyaramkumar7060@gmail.com>
2024-08-07 09:46:52 +0530

    added code files

--- app/src/main/java/com/example/diceapp/MainActivity.kt
+++ app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -73,7 +73,7 @@ fun DiceWithImage(modifier: Modifier=Modifier){
     Column(modifier=modifier,
         horizontalAlignment = Alignment.CenterHorizontally
         ) {
-                Image(painter = painterResource(imgResource) , contentDescription = "first",
+                Image(painter = painterResource(imgResource) , contentDescription = "second",
                     Modifier.height(440.dp))
                 Spacer(modifier = Modifier.height(86.dp))
                 Button(onClick = {
